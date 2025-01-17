04cdcc2aabc1d56f7b412e78186a729ebbb9cf81
superiorcollege740@gmail.com <superiorcollege740@gmail.com>
2025-01-17 05:59:23 +0500

    some changes in skills section

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ for(let i = 0 ; i<percent ; i++){
 
 });
 
+
+
+
 /* mix it up portfolio section */
 var mixer = mixitup(".portfolio-gallery");
 
@@ -74,6 +77,29 @@ window.location.href = "exploreprojects.html"
 }
 // <!-- header section -->
 
+function refreshpage(){
+    const circles = document.querySelectorAll(".circle")
+    circles.forEach(elem =>{
+        var dots = elem.getAttribute("data-dots");
+         var marked = elem.getAttribute("data-percent");
+         var percent = Math.floor(dots*marked/100);
+         var points = "";
+         var rotate = 360 / dots;
+         for(var i = 0; i < dots ; i++){
+            points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+         }
+    
+    elem.innerHTML = points;
+    
+    const pointsMarked = elem.querySelectorAll(".points");
+    for(let i = 0 ; i<percent ; i++){
+        pointsMarked[i].classList.add("marked")
+    }
+    
+    });
+}
+
+
 document.addEventListener('scroll' , ()=>{
     let menuIcon = document.querySelector("#menu-icon");
     let headfixed = document.getElementById("headfixed");
@@ -82,8 +108,8 @@ document.addEventListener('scroll' , ()=>{
         headfixed.classList.add("navjs");
       menuIcon.classList.add("menu-icon");
     }else{
-        headfixed.classList.remove("navjs")
-        menuIcon.classList.remove("menu-icon")
+        headfixed.classList.remove("navjs");
+        menuIcon.classList.remove("menu-icon");
     }
   })
   // <!-- navbar section -->
