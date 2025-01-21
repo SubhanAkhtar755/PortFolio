d6b7ab5240172e61d7a498170f59c1d10fc3d861
SubhanAkhtar <superiorcollege740@gmail.com>
2025-01-20 23:46:55 -0800

    error

--- exploreprojects.js
+++ exploreprojects.js
@@ -81,7 +81,7 @@ let portfolioArray = [
     
     portfolioArray.forEach((v,i)=>{
     
-        portfolioGallery.innerHTML += `  <div class="port-box mix ${v.classadd}">
+        portfolioGallery.innerHTML += `  <div class="port-box mix  ${v.classadd}">
                         <div class="port-image">
                             <img src="${v.img}" alt="">
                         </div>
@@ -93,12 +93,10 @@ let portfolioArray = [
                     </div>`
     
     })
+         
     
     
-    
-    
-    
-    
+     
     
     
     
@@ -135,6 +133,27 @@ let portfolioArray = [
     
     
     
+    /*  parallax --------------------------- */
+    
+    
+      const observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if(entry.isIntersecting){
+                entry.target.classList.add("show-items");
+            }  else{
+                entry.target.classList.remove("show-items");
+            }
+        });
+    });
+    
+    const scrollScale = document.querySelectorAll(".scroll-scale");
+    scrollScale.forEach((el)=>observer.observe(el));
+    
+    const scrollBottom = document.querySelectorAll(".scroll-bottom");
+    scrollBottom.forEach((el)=>observer.observe(el));
+    
+    const scrollTop= document.querySelectorAll(".scroll-top");
+    scrollTop.forEach((el)=>observer.observe(el));
     
     
     
