e5f4cfa225372b4e9c86f0f1859c158aa62c4ac6
SubhanAkhtar <superiorcollege740@gmail.com>
2025-01-24 01:35:24 +0500

    add curser

--- app.js
+++ app.js
@@ -211,3 +211,23 @@ let hireswal = Swal.fire("If any company hires me, they must contact me on Whats
     
 //         cv1 = cvswal === true ? cvswal : cvswal ;
 //     }
+
+
+// ! cursor section ----------------------------------------->>>>> //
+const cursorDot = document.querySelector('[data-cursor-dot]');
+const cursorOutline = document.querySelector('[data-cursor-outline]');
+
+
+window.addEventListener('mousemove', (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
+  cursorDot.style.left = `${posX}px`;
+   cursorDot.style.top = `${posY}px`;
+//    cursorOutline.style.left = `${posX}px`;
+//    cursorOutline.style.top = `${posY}px`;
+cursorOutline.animate({
+   left: `${posX}px`,
+   top: `${posY}px`,
+}, {duration: 500, fill: 'forwards'})
+
+});
